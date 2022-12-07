57e38469f7b2d0aaaeab26385c157f87f01723a8
JRRBorkowski <96788464+JRRBorkowski@users.noreply.github.com>
2022-12-07 09:37:45 +0100

    added reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  shopForm = this.builder.group({
+    storeName: this.builder.control(''),
+    industry: this.builder.control(''),
+    isRegistered: this.builder.control(false),
+    companyName: this.builder.control(''),
+    vat: this.builder.control(''),
+
+    location: this.builder.group({
+      adress: this.builder.control(''),
+      zipCode: this.builder.control(''),
+      city: this.builder.control(''),
+      country: this.builder.control(''),
+    }),
+  });
+
+  submitForm() {
+    console.log(this.shopForm.value);
+  }
+
+  constructor(private builder: FormBuilder) {}
+
+  ngOnInit(): void {}
 }
